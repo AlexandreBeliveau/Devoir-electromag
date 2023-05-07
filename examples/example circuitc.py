52b8461e7b4b9f074d0afb0e0dadcbc367ad4f84
Olivier Bourget <olbou102@ulaval.ca>
2023-05-07 13:06:19 -0400

    CircuitC

---
+++ examples/example circuitc.py
@@ -0,0 +1,52 @@
+import env_examples  # Modifies path, DO NOT REMOVE
+
+from sympy import Symbol
+
+from src import Circuit, CoordinateSystem, VoltageSource, Wire, World
+
+
+if __name__ == "__main__":
+    WORLD_SHAPE = (101, 101)
+    BATTERY_VOLTAGE = 1.0
+    HIGH_WIRE_RESISTANCE = 1.0
+    LOW_WIRE_RESISTANCE = 0.01
+
+    cartesian_variables = Symbol("x"), Symbol("y")
+    x, y = cartesian_variables
+
+    x_expression_vertical = 0 * x
+    y_expression_vertical = y
+    vertical_eqs = (x_expression_vertical, y_expression_vertical)
+
+    x_expression_horizontal = x
+    y_expression_horizontal = 0 * y
+    horizontal_eqs = (x_expression_horizontal, y_expression_horizontal)
+
+    x_expression_diagonal = x
+    y_expression_diagonal = y
+    diagonal_eqs = (x_expression_diagonal, y_expression_diagonal)
+    wires = [
+        #Wire((26, 60), (26, 74), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        #Wire((26, 74), (74, 74), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        #Wire((74, 74), (74, 60), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        #Wire((74, 60), (74, 40), vertical_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
+        #Wire((74, 40), (74, 26), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        #Wire((74, 26), (26, 26), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        #Wire((26, 26), (26, 40), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        #VoltageSource((26, 40), (26, 60), vertical_eqs, cartesian_variables, BATTERY_VOLTAGE)
+        VoltageSource((40, 26), (60, 26), horizontal_eqs, cartesian_variables, BATTERY_VOLTAGE),
+        Wire((60, 26), (74, 50), diagonal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((74, 50), (60, 74), diagonal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE), 
+        Wire((60, 74), (40, 74), horizontal_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE), 
+        Wire((40, 74), (26, 50), diagonal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE), 
+        Wire((26, 50), (40, 26), diagonal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE)
+    ]
+    ground_position = (40, 26)
+
+    circuit = Circuit(wires, ground_position)
+    world = World(circuit=circuit, coordinate_system=CoordinateSystem.CARTESIAN, shape=WORLD_SHAPE)
+    world.show_circuit(
+        {0: (40, 26), 1: (60, 26), 2: (74, 50), 3: (60, 74), 4: (40, 74), 5: (26, 50)}
+    )
+    world.compute()
+    world.show_all()
