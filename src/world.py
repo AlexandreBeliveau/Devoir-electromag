267fd7a956c36e28b5f34230a988654a879f4605
Alexandre Beliveau <alexandre.beliveau.2@ulaval.ca>
2023-05-13 13:23:31 -0400

    Code roule Biotsavard caca

--- src/world.py
+++ src/world.py
@@ -6,7 +6,7 @@ from scipy.constants import mu_0, pi
 from src.biot_savart_equation_solver import BiotSavartEquationSolver
 from src.circuit import Circuit
 from src.coordinate_and_position import CoordinateSystem, Position
-from src.fields import VectorField, ScalarField
+from src.fields import VectorField
 from src.laplace_equation_solver import LaplaceEquationSolver
 
 
@@ -150,6 +150,11 @@ class World:
         self._potential = pierresimon.solve(self._circuit_voltage, self._coordinate_system, self.delta_q1, self.delta_q2)
 
         self._electric_field = -self._potential.gradient()
+
+        Biot = BiotSavartEquationSolver()
+        self._magnetic_field = Biot.solve(self._circuit_current, self._coordinate_system, self.delta_q1, self.delta_q2)
+
+        self._energy_flux = 1/mu_0*(VectorField.cross(self._electric_field, self._magnetic_field))
 
     def show_circuit(self, nodes_position_in_figure: dict = None):
         """
